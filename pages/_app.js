bdf516d9343fb475ecc2f5180135ca667adbd092
Kicki Halmos <kickihalmos@gmail.com>
2021-11-26 11:24:42 +0100

    changle typo in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'tailwindcss/tailwind.css';
-import Layout from '../Components/Layout';
-import Nav from '../Components/Nav';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
 import { Provider } from 'react-redux';
 import store from '../store/index';
 
